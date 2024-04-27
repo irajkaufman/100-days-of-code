7881562749d0dddbdf43816967ed4dc70508ffcc
Ira Kaufman <ira.kaufman@gmail.com>
2024-04-26 17:34:58 -0700

    Update guess_number.py
    flipped all "print"s to "st.write"

--- pages/guess_number.py
+++ pages/guess_number.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from art import guess_number_logo
 import os
 import random as r
@@ -21,19 +22,19 @@ def difficulty_assignment():
         print("You have 5 attempts to guess the number.")
         return 5
     elif difficulty.lower() == "easy":
-        print("You have 10 attempts to guess the number.")
+        st.write("You have 10 attempts to guess the number.")
         return 10
     else:
-        print("That was an invalid response.")
+        st.write("That was an invalid response.")
         return 0
 
 
 # evaluate the guess
 def check_guesses(user_guess, correct_number):
     if user_guess > correct_number:
-        print("Too high.")
+        st.write("Too high.")
     elif user_guess < correct_number:
-        print("Too low.")
+        st.write("Too low.")
     elif user_guess == correct_number:
         return True
 
@@ -41,7 +42,7 @@ def check_guesses(user_guess, correct_number):
 def exit_gracefully(result, correct_number):
     # Call the function to clear the console
     clear_console()
-    print(guess_number_logo)
+    st.write(guess_number_logo)
     if result == 0:
         return f"Sorry, but you've run out of guesses.\nThe winning number was {correct_number}.\nThanks for playing!"
     else:
@@ -51,24 +52,24 @@ def exit_gracefully(result, correct_number):
 def game():
     # beginning housekeeping
     clear_console()
-    print(guess_number_logo)
+    st.write(guess_number_logo)
 
     # establish winning number
     winning_number = r.randint(1, 100)
 
     # intro and welcome
-    print("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
-    # print(f"Pssst, the correct answer is {str(winning_number)}")
+    st.write("Welcome to the Number Guessing Game!\nI'm thinking of a number between 1 and 100.")
+    # st.write(f"Pssst, the correct answer is {str(winning_number)}")
 
     # determine number of guesses based on difficulty level chosen in function, above
     max_guesses = difficulty_assignment()
 
     # handle for invalid response to difficulty level check
     if max_guesses == 0:
-        print("Please try again.")
+        st.write("Please try again.")
         max_guesses = difficulty_assignment()
         if max_guesses == 0:
-            print("Sorry, but you were given two opportunities to provide a valid response, \
+            st.write("Sorry, but you were given two opportunities to provide a valid response, \
             and did not do so either time.\nHave a nice day!")
 
     # loop to take guesses
@@ -81,17 +82,17 @@ def game():
         else:
             max_guesses -= 1
         if max_guesses > 0:
-            print("Guess again.")
+            st.write("Guess again.")
             if max_guesses > 1:
-                print(f"You have {max_guesses} attempts remaining to guess the number.")
+                st.write(f"You have {max_guesses} attempts remaining to guess the number.")
             else:
-                print("You have 1 attempt remaining to guess the number.")
+                st.write("You have 1 attempt remaining to guess the number.")
 
     # exit gracefully
     ending = exit_gracefully(max_guesses, winning_number)
-    print(ending)
-    print()
-    print()
+    st.write(ending)
+    st.write()
+    st.write()
 
 
 if __name__ == "__guess_number__":
